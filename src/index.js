ea668da127eb15b37f350e5bd9d271a69103899f
cemna <lkst_enma_daioh@hotmail.com>
2018-09-18 18:11:01 -0300

    refactored board's render method to remove repetition by building the game's squares through loops

--- src/index.js
+++ src/index.js
@@ -24,27 +24,24 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} 
+    return <Square key={i} value={this.props.squares[i]} 
     			   clickCallback={() => this.props.clickCallback(i)} />;
   }
   render() {
+    let squareDivs = [];
+    let parentDivs = [];
+    let index      = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        squareDivs = squareDivs.concat(this.renderSquare(index));
+        index++;
+      }
+      parentDivs = parentDivs.concat(<div key={i} className="board-row">{squareDivs}</div>);
+      squareDivs = [];
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {parentDivs}
       </div>
     );
   }
